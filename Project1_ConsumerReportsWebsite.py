668562a3fdfd4d4ed23c05fc1c09d8b2cc35b667
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-03 01:36:17 +0000

    feat(Project1): access major links for scraping

--- Project1_ConsumerReportsWebsite.py
+++ Project1_ConsumerReportsWebsite.py
@@ -24,8 +24,13 @@ for card in cards[1:]:
     if link_tag:
         # Get the text
         text = link_tag.get_text(strip=True)
-        print(text)
         # Get the full link
         link = base_url + link_tag['href']
         link_list.append(link)
-        print(link)
+
+# in-this-article list
+for link in link_list:
+    response = requests.get(link, timeout=20, headers=headers)
+    soup = BeautifulSoup(response.content, "html5lib")
+    in_this_article = soup.find_all('div', attrs={'id': 'in-this-article-3'})
+    print(in_this_article)
