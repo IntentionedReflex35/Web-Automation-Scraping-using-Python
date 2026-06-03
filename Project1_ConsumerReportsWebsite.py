f0446fb63c2b7c97c726c0689b85cd74d8eb7e32
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-03 01:16:48 +0000

    feat(Project1): find element and tag extract text and links

--- Project1_ConsumerReportsWebsite.py
+++ Project1_ConsumerReportsWebsite.py
@@ -1,4 +1,5 @@
 # Project 1: Customer Reports/ Consumer Reports
+# Import libraries
 import requests
 import html5lib
 from bs4 import BeautifulSoup
@@ -8,10 +9,23 @@ headers = {"User-Agent": "Mozilla/5.0 "
                          "AppleWebKit/537.36 (KHTML, likeGecko) "
                          "Chrome/119.0.0.0 Safari/537.36"
            }
+
+# Get response
 base_url = "https://www.consumerreports.org/"
-
 response = requests.get(base_url, timeout=20, headers=headers)
-
 soup = BeautifulSoup(response.content, "html5lib")
 
-print(soup.prettify())
+# Accessing the url
+cards = soup.find_all('div', attrs={'class': 'border-t border-gray-light first:border-t-0 group'})
+link_list = []      # Store all links here
+
+for card in cards[1:]:
+    link_tag = card.find('a')
+    if link_tag:
+        # Get the text
+        text = link_tag.get_text(strip=True)
+        print(text)
+        # Get the full link
+        link = base_url + link_tag['href']
+        link_list.append(link)
+        print(link)
