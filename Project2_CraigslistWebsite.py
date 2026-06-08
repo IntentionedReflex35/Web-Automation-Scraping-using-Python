099c1437704e0b0eae68465ef8ef9218424e5e8d
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-08 02:24:30 +0000

    refactor(Project2): remove html5lib library

--- Project2_CraigslistWebsite.py
+++ Project2_CraigslistWebsite.py
@@ -2,7 +2,6 @@
 
 # Import libraries
 import requests
-import html5lib
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -18,4 +17,34 @@ response = requests.get(base_url, timeout=20, headers=headers)
 soup = BeautifulSoup(response.content, "html5lib")
 
 # Get content
-print(soup.prettify())
+# print(soup.prettify())
+
+# Find element
+search_results = soup.find_all('li', class_='cl-static-search-result')
+extracted_data = []      # Empty list data addition
+
+# Loop through the results and extract the title, price , location and link for each listing
+for result in search_results:
+    # Extract link
+    link_tag = result.find('a')
+    link = link_tag['href'] if link_tag else None
+    # Extract title
+    title = result.get_text(strip=True)
+    # Extract price
+    price_tag = result.find('div', class_='price')
+    price = price_tag.get_text(strip=True)
+    # Extract location
+    location_tag = result.find('div', class_="location")
+    location = location_tag.get_text(strip=True)
+
+    extracted_data.append({"Name": title,
+                           "Link": link,
+                           "Location": location,
+                           "Price": price})
+
+# print(extracted_data)
+
+# Save DataFrame to CSV file
+df = pd.DataFrame(extracted_data)
+print(df)
+df.to_csv('accra_craigslist.csv', index=False)
